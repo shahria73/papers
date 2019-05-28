29a32403f6ce34aa433127a18ac0466ec1e0c229
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 13:36:58 +0000

    Deploy HDR-UK/papers to github.com/HDR-UK/papers.git:gh-pages

--- paper-extractor.py
+++ paper-extractor.py
@@ -10,7 +10,7 @@ import json
 import urllib
 import requests
 
-EPMC_BASE_URL="https://www.ebi.ac.uk/europepmc/webservices/rest/search?pageSize=1000&format=json&"
+EPMC_BASE_URL="https://www.ebi.ac.uk/europepmc/webservices/rest/search?resultType=core&pageSize=1000&format=json&"
 QUERY="ACK_FUND:\"HDRUK\" OR ACK_FUND:\"HDR UK\" OR ACK_FUND:\"HDR-UK\" OR ACK_FUND:\"Health Data Research UK\""
 
 DATA = []
@@ -33,18 +33,25 @@ def retrieve_papers(data=DATA, query=QUERY, cursorMark="*"):
     retrieve_papers(DATA, cursorMark=d['nextCursorMark'])
 
 def export_csv():
-  column_names = ['id', 'doi', 'title', 'authorString', 'journalTitle', 'pubYear']
+  column_names = ['id', 'doi', 'title', 'authorString', 'authorAffiliations', 'journalTitle', 'pubYear', 'abstract']
   with open('data/papers.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=column_names)
     writer.writeheader()
     for d in DATA:
+      # Extracting Author affiliations
+      authorAffiliations = []
+      for author in d['authorList']['author']:
+        if 'affiliation' in author.keys():
+          authorAffiliations.append(author['affiliation'])
       row = {
-        'id': d['id'],
-        'doi': d['doi'],
+        'id': "http://europepmc.org/articles/" + d['pmcid'],
+        'doi': "https://doi.org/" + d['doi'],
         'title': d['title'],
         'authorString': d['authorString'],
-        'journalTitle': d['journalTitle'],
-        'pubYear': d['pubYear']
+        'authorAffiliations': authorAffiliations,
+        'journalTitle': d['journalInfo']['journal']['title'],
+        'pubYear': d['pubYear'],
+        'abstract': d['abstractText']
       }
       writer.writerow(row)
 
