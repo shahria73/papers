64291e7673e9058097df35db03a06a595e0e40df
Susheel Varma <susheel.varma@gmail.com>
2020-07-13 10:45:36 +0100

    Changing author email

--- preprint-extractor.py
+++ preprint-extractor.py
@@ -2,7 +2,7 @@
 # usage: preprint-extractor.py
 __author__ = "Susheel Varma"
 __copyright__ = "Copyright (c) 2020 Susheel Varma All Rights Reserved."
-__email__ = "susheel.varma@ebi.ac.uk"
+__email__ = "susheel.varma@hdruk.ac.uk"
 __license__ = "MIT"
 
 import csv
