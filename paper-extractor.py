f440635e26d42008b14148028521e65374243116
Susheel Varma <susheel.varma@gmail.com>
2020-07-24 16:17:15 +0100

    Simplifying paper extraction

--- paper-extractor.py
+++ paper-extractor.py
@@ -12,11 +12,9 @@ import requests
 from pprint import pprint
 
 EPMC_BASE_URL = "https://www.ebi.ac.uk/europepmc/webservices/rest/search?resultType=core&pageSize=1000&format=json&"
-SEARCH_TEXT = ['HDRUK', 'HDR UK', 'HDR-UK', 'Health Data Research UK']
-ACK_FUND_QUERY = " OR ".join(["ACK_FUND:\"{}\"".format(t) for t in SEARCH_TEXT])
-AFF_QUERY = " OR ".join(["AFF:\"{}\"".format(t) for t in SEARCH_TEXT])
 
-COVID_QUERY = "(\"2019-nCoV\" OR \"2019nCoV\" OR \"COVID-19\" OR \"SARS-CoV-2\" OR \"COVID19\" OR \"COVID\" OR \"SARS-nCoV\" OR (\"wuhan\" AND \"coronavirus\") OR \"Coronavirus\" OR \"Corona virus\" OR \"corona-virus\" OR \"corona viruses\" OR \"coronaviruses\" OR \"SARS-CoV\" OR \"Orthocoronavirinae\" OR \"MERS-CoV\" OR \"Severe Acute Respiratory Syndrome\" OR \"Middle East Respiratory Syndrome\" OR (\"SARS\" AND \"virus\") OR \"soluble ACE2\" OR (\"ACE2\" AND \"virus\") OR (\"ARDS\" AND \"virus\") or (\"angiotensin-converting enzyme 2\" AND \"virus\")) AND ((ACK_FUND:\"HDRUK\" OR ACK_FUND:\"HDR UK\" OR ACK_FUND:\"HDR-UK\" OR ACK_FUND:\"Health Data Research UK\") OR (AFF:\"HDRUK\" OR AFF:\"HDR UK\" OR AFF:\"HDR-UK\" OR AFF:\"Health Data Research UK\"))"
+HDRUK_PAPERS_QUERY = "((ACK_FUND:\"HDRUK\" OR ACK_FUND:\"HDR UK\" OR ACK_FUND:\"HDR-UK\" OR ACK_FUND:\"Health Data Research UK\") OR (AFF:\"HDRUK\" OR AFF:\"HDR UK\" OR AFF:\"HDR-UK\" OR AFF:\"Health Data Research UK\")) AND NOT (SRC:PPR)"
+COVID_PAPERS_QUERY = "(\"2019-nCoV\" OR \"2019nCoV\" OR \"COVID-19\" OR \"SARS-CoV-2\" OR \"COVID19\" OR \"COVID\" OR \"SARS-nCoV\" OR (\"wuhan\" AND \"coronavirus\") OR \"Coronavirus\" OR \"Corona virus\" OR \"corona-virus\" OR \"corona viruses\" OR \"coronaviruses\" OR \"SARS-CoV\" OR \"Orthocoronavirinae\" OR \"MERS-CoV\" OR \"Severe Acute Respiratory Syndrome\" OR \"Middle East Respiratory Syndrome\" OR (\"SARS\" AND \"virus\") OR \"soluble ACE2\" OR (\"ACE2\" AND \"virus\") OR (\"ARDS\" AND \"virus\") or (\"angiotensin-converting enzyme 2\" AND \"virus\")) AND ((ACK_FUND:\"HDRUK\" OR ACK_FUND:\"HDR UK\" OR ACK_FUND:\"HDR-UK\" OR ACK_FUND:\"Health Data Research UK\") OR (AFF:\"HDRUK\" OR AFF:\"HDR UK\" OR AFF:\"HDR-UK\" OR AFF:\"Health Data Research UK\")) AND NOT (SRC:PPR)"
 
 # HDR UK Custom tags
 NATIONAL_PRIORITIES_CSV = "data/national-priorities.csv"
@@ -136,26 +134,16 @@ def merge(key, *lists):
 
 
 def main():
-  # retrieve papers with funding acknowledgement to HDR-UK
-  ack_data = retrieve_papers(query=ACK_FUND_QUERY, data=[])
-  data, header = format_data(ack_data)
-  export_csv(data, header, 'data/acknowledgements.csv')
-
-  # retrieve papers with author affiliation to HDR-UK
-  aff_data = retrieve_papers(query=AFF_QUERY, data=[])
-  data, header = format_data(aff_data)
-  export_csv(data, header, 'data/affiliations.csv')
-  
-  # retrieve COVID-19 papers with author affiliation or funding acknowledgement to HDR-UK
-  covid_data = retrieve_papers(query=COVID_QUERY, data=[])
-  data, header = format_data(covid_data)
-  export_csv(data, header, 'data/covid-papers.csv')
-
-  # export papers with author affiliation OR funding acknowledgement to HDR-UK
-  mergedData = merge('id', ack_data, aff_data)
-  data, header = format_data(mergedData)
+  # retrieve papers with author affiliation or funding acknowledgement to HDR-UK
+  papers = retrieve_papers(query=HDRUK_PAPERS_QUERY, data=[])
+  data, header = format_data(papers)
   export_csv(data, header, 'data/papers.csv')
   export_json(data, 'data/papers.json')
+  
+  # retrieve COVID-19 papers with author affiliation or funding acknowledgement to HDR-UK
+  covid_papers = retrieve_papers(query=COVID_PAPERS_QUERY, data=[])
+  data, header = format_data(covid_papers)
+  export_csv(data, header, 'data/covid-papers.csv')
 
 
 if __name__ == "__main__":
